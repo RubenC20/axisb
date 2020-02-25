2efeecac20f275a184c5891e35689b648a62f172
Andreas Mang <andreas@math.uh.edu>
2020-02-24 20:51:58 -0600

    homework 10 template updated

--- homework/hw10.m
+++ homework/hw10.m
@@ -20,5 +20,8 @@ fprintf('error for rref      dr = ||Ax - b||_2 = %e\n', dr);
 
 % compute solution of system using backslash command
 x = % ADD YOUR CODE HERE
+
+% compute the error and show it
+dr = % ADD YOUR CODE HERE
 fprintf('error for backslash dr = ||Ax - b||_2 = %e\n', dr);
 fprintf('\n');
