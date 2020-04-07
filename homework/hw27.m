07c9310ea33fa9fb2e5a0bc3f17a1037f2d3be8b
Andreas Mang <andreas@math.uh.edu>
2020-04-07 15:29:43 -0500

    new homework added

---
+++ homework/hw27.m
@@ -0,0 +1,25 @@
+clear, close all; clc;
+
+% part a)
+% number of rows and cols of identity matrix
+n = 5;
+
+% define given vectors
+v1 = [-9;-7; 8;-5; 7];
+v2 = [ 9; 4; 1; 6;-7];
+v3 = [ 6; 7;-8; 5;-7];
+
+% construct identity matrix
+I = % YOUR CODE GOES HERE
+
+% construct matrix A
+A = % YOUR CODE GOES HERE
+
+
+% observations:
+% YOUR OBSERVATIONS GO HERE
+
+
+% part b)
+% explanation:
+% YOUR EXPLANATION GOES HERE
